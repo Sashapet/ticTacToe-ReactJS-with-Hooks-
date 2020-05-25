200d0baf8fc6c82310ad6f08b1805dd6c6791131
Sashapet <fastzumftwlols@gmail.com>
2020-05-25 23:20:36 +0300

    Updates

---
+++ precache-manifest.28437ba546ff4800f486f3e330302daf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "babe4ef4aa6438ab75eb0c3b1f11b1b0",
+    "url": "/ticTacToe-ReactJS-with-Hooks-/index.html"
+  },
+  {
+    "revision": "3fa23deee07fa7f77758",
+    "url": "/ticTacToe-ReactJS-with-Hooks-/static/js/2.593cd1f7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ticTacToe-ReactJS-with-Hooks-/static/js/2.593cd1f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e107f8d8515abdf9bf35",
+    "url": "/ticTacToe-ReactJS-with-Hooks-/static/js/main.87dadf15.chunk.js"
+  },
+  {
+    "revision": "1097d0f28ea880dd030d",
+    "url": "/ticTacToe-ReactJS-with-Hooks-/static/js/runtime-main.e5b9a58a.js"
+  }
+]);
