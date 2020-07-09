c303c84bbc35f2f9aec182489e6d30e92e48dac2
Sashapet <fastzumftwlols@gmail.com>
2020-07-09 17:31:14 +0300

    Updates

---
+++ precache-manifest.2a5f6e27929f9e40e6c1bfc923e8a248.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e548ec9d4a7411cfa6124a60ebd49b5a",
+    "url": "/ticTacToe-ReactJS-with-Hooks-/index.html"
+  },
+  {
+    "revision": "7d4dbab770dff5432a53",
+    "url": "/ticTacToe-ReactJS-with-Hooks-/static/css/main.b125e38d.chunk.css"
+  },
+  {
+    "revision": "b4e41b1a939c5f7c1e2b",
+    "url": "/ticTacToe-ReactJS-with-Hooks-/static/js/2.cb39cddd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ticTacToe-ReactJS-with-Hooks-/static/js/2.cb39cddd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d4dbab770dff5432a53",
+    "url": "/ticTacToe-ReactJS-with-Hooks-/static/js/main.f7703ae2.chunk.js"
+  },
+  {
+    "revision": "1097d0f28ea880dd030d",
+    "url": "/ticTacToe-ReactJS-with-Hooks-/static/js/runtime-main.e5b9a58a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ticTacToe-ReactJS-with-Hooks/precache-manifest.a8e5d0394789ce8dfde46b8f954d54d3.js"
+  "/ticTacToe-ReactJS-with-Hooks-/precache-manifest.2a5f6e27929f9e40e6c1bfc923e8a248.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ticTacToe-ReactJS-with-Hooks/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ticTacToe-ReactJS-with-Hooks-/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
